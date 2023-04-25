b59df55f829e15395288b3781ea2c674811a63c3
Yi Ying Li <jupyter-yiyingl4@andrew.cm-9d9fb@jupyterhub-dev.cheme.cmu.edu>
2023-04-25 06:05:19 +0000

    black reformat

--- pkg/.ipynb_checkpoints/setup-checkpoint.py
+++ pkg/.ipynb_checkpoints/setup-checkpoint.py
@@ -1,11 +1,14 @@
 from setuptools import setup
 
-setup(name='final',
-      version='0.0.1',
-      description='final utilities',
-      maintainer='Yi Ying Li',
-      maintainer_email='yiyingl4@andrew.cmu.edu',
-      license='MIT',
-      packages=['final'],
-      scripts=[],
-      long_description='''A set of bibtex utilities''')
+setup(
+    name="final",
+    version="0.0.1",
+    description="final utilities",
+    maintainer="Yi Ying Li",
+    maintainer_email="yiyingl4@andrew.cmu.edu",
+    license="MIT",
+    packages=["final"],
+    scripts=[],
+    entry_points={"console_scripts": ["oa = final.main:main"]},
+    long_description="""A set of bibtex utilities""",
+)
